85f1fe681be4cc05352b54331c356200f77dfa0b
JDawnF <335825732@qq.com>
2019-07-17 23:56:19 +0800

    启动类及配置

---
+++ yuehu_spit/src/main/java/com/baichen/spit/SpitApplication.java
@@ -0,0 +1,22 @@
+package com.baichen.spit;
+
+import com.baichen.util.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @Program: SpitApplication
+ * @Author: baichen
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApplication.class, args);
+    }
+    @Bean
+    public IdWorker idWorkker(){
+        return new IdWorker(1, 1);
+    }
+}
