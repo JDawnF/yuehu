b7c20215124cd1e08db511a7428d291655d05890
JDawnF <335825732@qq.com>
2019-07-26 23:57:46 +0800

    引入Eureka 并创建server

---
+++ yuehu_eureka/src/main/java/com/baichen/eureka/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.baichen.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Program: EurekaServer
+ * @Author: baichen
+ * @Description: 启动类
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
